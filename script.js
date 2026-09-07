5c4f5661eb343e660215f008dae06e025386a929
Amer <203231960+Anthrax-2@users.noreply.github.com>
2026-09-07 23:40:49 +0300

    Initialize some variables to access HTML elements

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+
+const selectField = document.querySelector("#difficulty")
+const inputField = document.querySelector("#number")
+const outputField = document.querySelector("#output")
+
+
