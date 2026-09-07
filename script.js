a37b4a2cfae2bbb2f35a3f8904297bd87d8cdc38
Amer <203231960+Anthrax-2@users.noreply.github.com>
2026-09-08 00:02:38 +0300

    Initialze logic to track the chosen difficulty and determine the number of rounds associated with it

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ const selectField = document.querySelector("#difficulty")
 const inputField = document.querySelector("#number")
 const outputField = document.querySelector("#output")
 
+const roundsPerDifficulty = {"1": "10", "2": "5", "3": "2"}
+
+let pickedDifficulty = null
+let numberOfRounds = null 
+
+selectField.addEventListener("change", (event) => {
+    event.preventDefault()
+    pickedDifficulty = selectField.value
+    numberOfRounds = Number(roundsPerDifficulty[pickedDifficulty])
+})
+
+
+
+
+
+
 
